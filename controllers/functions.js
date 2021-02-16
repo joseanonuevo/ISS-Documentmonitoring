84c0b488b4701a9bbe7fd8bc7e935ec9b16f60fd
Jose Anonuevo <josecornado.anonuevo.iics@ust.edu.ph>
2021-02-17 07:46:08 +0800

    balik sa luma

--- controllers/functions.js
+++ controllers/functions.js
@@ -2,17 +2,27 @@ const db = require("../db/connectDB");
 const bcrypt = require("bcrypt");
 
 exports.changePw = (req, res) => {
-  var password = document.getElementById("password"),
-    confirm_password = document.getElementById("confirm_password");
+  const { oldpassword, newpassword } = req.body;
 
-  function validatePassword() {
-    if (password.value != confirm_password.value) {
-      confirm_password.setCustomValidity("Passwords Don't Match");
+  sql1 = "SELECT user_Password FROM users WHERE user_ID = ?";
+
+  db.query(sql1, [id], async (err, results) => {
+    oldpw = results[0].user_Password;
+    if (
+      (await bcrypt.compare(oldpassword, results[0].user_Password)) === false
+    ) {
+      res.send("Wrong password");
     } else {
-      confirm_password.setCustomValidity("");
+      // "UPDATE users SET user_Password = ? WHERE user_Email = ?"
+      const hashedPassword = await bcrypt.hash(newpassword, 5);
+      newsql = "UPDATE users SET user_Password = ? WHERE user_ID = ?";
+      db.query(newsql, [hashedPassword, id], (err, results) => {
+        if (!err) {
+          res.send("Updated");
+        } else {
+          res.send(err);
+        }
+      });
     }
-  }
-
-  password.onchange = validatePassword;
-  confirm_password.onkeyup = validatePassword;
+  });
 };
