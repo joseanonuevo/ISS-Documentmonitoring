82b04a2df06e2fa09dcc229969f2f2d5542030d8
Jose Anonuevo <josecornado.anonuevo.iics@ust.edu.ph>
2021-02-17 08:09:21 +0800

    dddd

--- controllers/functions.js
+++ controllers/functions.js
@@ -2,27 +2,35 @@ const db = require("../db/connectDB");
 const bcrypt = require("bcrypt");
 
 exports.changePw = (req, res) => {
-  const { oldpassword, newpassword } = req.body;
+  try {
+    const { oldpassword, newpassword, newpassword1 } = req.body;
+    console.log(req.body);
+    const id = req.cookies.authcookie2;
 
-  sql1 = "SELECT user_Password FROM users WHERE user_ID = ?";
-
-  db.query(sql1, [id], async (err, results) => {
-    oldpw = results[0].user_Password;
-    if (
-      (await bcrypt.compare(oldpassword, results[0].user_Password)) === false
-    ) {
-      res.send("Wrong password");
-    } else {
-      // "UPDATE users SET user_Password = ? WHERE user_Email = ?"
-      const hashedPassword = await bcrypt.hash(newpassword, 5);
-      newsql = "UPDATE users SET user_Password = ? WHERE user_ID = ?";
-      db.query(newsql, [hashedPassword, id], (err, results) => {
-        if (!err) {
-          res.send("Updated");
-        } else {
-          res.send(err);
-        }
-      });
+    if (newpassword !== newpassword1) {
+      throw error;
     }
-  });
+    sql1 = "SELECT user_Password FROM users WHERE user_ID = ?";
+    db.query(sql1, [id], async (err, results) => {
+      oldpw = results[0].user_Password;
+      if (
+        (await bcrypt.compare(oldpassword, results[0].user_Password)) === false
+      ) {
+        res.send("Wrong password");
+      } else {
+        // "UPDATE users SET user_Password = ? WHERE user_Email = ?"
+        const hashedPassword = await bcrypt.hash(newpassword, 5);
+        newsql = "UPDATE users SET user_Password = ? WHERE user_ID = ?";
+        db.query(newsql, [hashedPassword, id], (err, results) => {
+          if (!err) {
+            res.send("Updated");
+          } else {
+            res.send(err);
+          }
+        });
+      }
+    });
+  } catch {
+    return res.send("Passwords do not match");
+  }
 };
