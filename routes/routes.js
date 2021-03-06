a228e6135c2996bd833b8245d40d790f3232307d
jerichomarc <jerichomarc.reodique.iics@ust.edu.ph>
2021-03-06 11:40:09 +0800

    Notif on all pages & fixed alert on not an admin
    golteb

--- routes/routes.js
+++ routes/routes.js
@@ -30,7 +30,8 @@ router.get("/adminHome", verify, isAdmin, (req, res) => {
   const sql = "SELECT * from create_document WHERE status = 1";
   db.query(sql, (err, results1) => {
     if (!err) {
-      const sql2 = "SELECT * FROM activity_log ORDER BY date DESC";
+      const sql2 =
+        "SELECT * FROM activity_log INNER JOIN users ON users.user_ID = activity_log.user_id ORDER BY date DESC";
       db.query(sql2, (err, results2) => {
         res.render("adminHome", {
           names: results1,
@@ -45,7 +46,8 @@ router.get("/archive", verify, (req, res) => {
   const sql = "SELECT * from create_document WHERE status = 0";
   db.query(sql, (err, results1) => {
     if (!err) {
-      const sql2 = "SELECT * FROM activity_log";
+      const sql2 =
+        "SELECT * FROM activity_log INNER JOIN users ON users.user_ID = activity_log.user_id ORDER BY date DESC";
       db.query(sql2, (err, results2) => {
         res.render("archive", {
           names: results1,
@@ -60,9 +62,10 @@ router.get("/adminArchive", verify, isAdmin, (req, res) => {
   const sql = "SELECT * from create_document WHERE status = 0";
   db.query(sql, (err, results1) => {
     if (!err) {
-      const sql2 = "SELECT * FROM activity_log";
+      const sql2 =
+        "SELECT * FROM activity_log INNER JOIN users ON users.user_ID = activity_log.user_id ORDER BY date DESC";
       db.query(sql2, (err, results2) => {
-        res.render("archiveArchive", {
+        res.render("adminArchive", {
           names: results1,
           activities: results2,
         });
@@ -75,7 +78,8 @@ router.get("/update/:id", verify, (req, res) => {
   const sql = "SELECT * from update_document WHERE createDocu_ID = ?";
   db.query(sql, [req.params.id], (err, results1) => {
     if (!err) {
-      const sql2 = "SELECT * FROM activity_log";
+      const sql2 =
+        "SELECT * FROM activity_log INNER JOIN users ON users.user_ID = activity_log.user_id ORDER BY date DESC";
       db.query(sql2, (err, results2) => {
         res.render("docUpdates", {
           names: results1,
@@ -96,9 +100,10 @@ router.get("/adminUpdate/:id", verify, isAdmin, (req, res) => {
   const sql = "SELECT * from update_document WHERE createDocu_ID = ?";
   db.query(sql, [req.params.id], (err, results1) => {
     if (!err) {
-      const sql2 = "SELECT * FROM activity_log";
+      const sql2 =
+        "SELECT * FROM activity_log INNER JOIN users ON users.user_ID = activity_log.user_id ORDER BY date DESC";
       db.query(sql2, (err, results2) => {
-        res.render("adminArchive", {
+        res.render("adminDocUpdates", {
           names: results1,
           activities: results2,
         });
@@ -119,7 +124,8 @@ router.get("/registeredUsers", verify, isAdmin, (req, res) => {
   const sql = "SELECT * FROM users where user_status = 0";
   db.query(sql, (err, results1) => {
     if (!err) {
-      const sql2 = "SELECT * FROM activity_log";
+      const sql2 =
+        "SELECT * FROM activity_log INNER JOIN users ON users.user_ID = activity_log.user_id ORDER BY date DESC";
       db.query(sql2, (err, results2) => {
         res.render("registeredUsers", {
           names: results1,
@@ -134,7 +140,8 @@ router.get("/disabledUsers", verify, isAdmin, (req, res) => {
   const sql = "SELECT * FROM users where user_status = 1";
   db.query(sql, (err, results1) => {
     if (!err) {
-      const sql2 = "SELECT * FROM activity_log";
+      const sql2 =
+        "SELECT * FROM activity_log INNER JOIN users ON users.user_ID = activity_log.user_id ORDER BY date DESC";
       db.query(sql2, (err, results2) => {
         res.render("disabledUsers", {
           names: results1,
@@ -172,7 +179,10 @@ function isAdmin(req, res, next) {
     if (result[0].isAdmin === 1) {
       next();
     } else {
-      res.send("not an admin");
+      return res.status(400).render("alertPage", {
+        wrongOldPw: "Not an Admin!",
+        error: "error",
+      });
     }
   });
 }
