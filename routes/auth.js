57c9d234b3cc4b39c0b31295458859f13eed07d4
Jose Anonuevo <josecornado.anonuevo.iics@ust.edu.ph>
2021-03-16 21:16:01 +0800

    tapos na

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const limitter = require("express-rate-limit");
+
+const loginLimitter = limitter({
+  windowMs: 60000 * 60,
+  max: 10,
+  message: "Too attempts, please try again after an hour",
+});
 
 const { login } = require("../controllers/auth");
 
-router.post("/login", login);
+router.post("/login", loginLimitter, login);
 
 module.exports = router;
