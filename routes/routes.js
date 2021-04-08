3bd03aceab51428bca06722835386ec473289b95
joseanonuevo <josecornado.anonuevo.iics@ust.edu.ph>
2021-04-08 16:55:32 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -6,15 +6,6 @@ const jwt = require('jsonwebtoken');
 const db = require('../db/connectDB');
 
 router.get('/', (req, res) => {
-	/*
-	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-	console.log(fullUrl);
-	if (fullUrl == 'http://iss-emonitor.org/' || 'http://localhost:3000/') {
-		return res.status(200).redirect('https://iss-emonitor.org/login');	
-	} else {
-		return res.status(200).redirect('https://iss-emonitor.org/login');
-	}
-	*/
 	res.render('login');
 });
 
