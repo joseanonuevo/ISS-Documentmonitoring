400d46c6e1d56344a38181162721632475e29f78
joseanonuevo <josecornado.anonuevo.iics@ust.edu.ph>
2021-05-01 00:56:58 +0800

    Update connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,28 +1,16 @@
 const mysql = require('mysql');
 
-const db_config = {
+const db = mysql.createPool({
 	host: process.env.DATABASE_HOST,
 	user: process.env.DATABASE_USER,
 	password: process.env.DATABASE_PASSWORD,
 	database: process.env.DATABASE
-};
-
-function handleDisconnect() {
-	db = mysql.createConnection(db_config);
-	db.connect((err) => {
-		if (err) {
-			setTimeout(handleDisconnect, 5000);
-		}
-	});
-	db.on('error', (err) => {
-		console.log('db error', err);
-		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-			handleDisconnect();
-		} else {
-			throw err;
-		}
-	});
-}
-handleDisconnect();
+});
+/*
+db.connect((err) => {
+	if (!err) return console.log('connected to ' + process.env.DATABASE);
+	else return console.log('connection error');
+});
+*/
 
 module.exports = db;
