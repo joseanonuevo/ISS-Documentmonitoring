783e68338192887f7ec5147e4ac164ba40b4cf45
joseanonuevo <josecornado.anonuevo.iics@ust.edu.ph>
2021-04-30 23:29:59 +0800

    dd

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,14 +1,14 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
+	host: process.env.DATABASE_HOST,
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE
 });
 db.connect((err) => {
-  if (!err) return console.log("connected to " + process.env.DATABASE);
-  else return console.log("connection error");
+	if (!err) return console.log('connected to ' + process.env.DATABASE);
+	else return console.log('connection error');
 });
 
 module.exports = db;
